b626f52162861d5791f4b8a56d631ccf16095573
ndutagrace25 <ndutagrace25@gmail.com>
2024-03-15 10:35:27 +0300

    finalized on the prototype

--- src/common/PurchasedToken.tsx
+++ src/common/PurchasedToken.tsx
@@ -1,25 +1,31 @@
-const PurchasedToken = () => {
+import { PurchasedTokenTypes } from "../pages/PurchasedTokens";
+
+interface Props {
+  item: PurchasedTokenTypes;
+  key: number;
+}
+const PurchasedToken = ({ item, key }: Props) => {
   return (
-    <div className="shadow-sm rounded p-3 bg-white mb-3">
+    <div className="shadow-sm rounded p-3 bg-white mb-3" key={key}>
       <div className="d-flex justify-content-between mb-2 ">
         <div>Meter:</div>
-        <div className="meter-title">37185698745</div>
+        <div className="meter-title">{item.meter_number}</div>
       </div>
       <div className="d-flex justify-content-between ">
         <div>Token:</div>
-        <div className="meter-title mb-2">1475-0553-400-5370-9209</div>
+        <div className="meter-title mb-2">{item.tokens}</div>
       </div>
       <div className="d-flex justify-content-between ">
         <div>Date:</div>
-        <div className="meter-title mb-2">2024-03-10</div>
+        <div className="meter-title mb-2">{item.date}</div>
       </div>
       <div className="d-flex justify-content-between ">
         <div>Units:</div>
-        <div className="meter-title mb-2">70.16</div>
+        <div className="meter-title mb-2">{item.units}</div>
       </div>
       <div className="d-flex justify-content-between ">
         <div>Amount:</div>
-        <div className="meter-title mb-2">KES 2000</div>
+        <div className="meter-title mb-2">KES {item.amount}</div>
       </div>
     </div>
   );
--- src/pages/PurchasedTokens.tsx
+++ src/pages/PurchasedTokens.tsx
@@ -5,15 +5,49 @@ import { PurchasedToken } from "../common";
 
 dayjs.extend(customParseFormat);
 
+export interface PurchasedTokenTypes {
+  meter_number: string;
+  tokens: string;
+  date: string;
+  units: string;
+  amount: string;
+}
+
 const PurchasedTokens = () => {
   const dateFormat = "YYYY-MM-DD";
+
+  const tokenPurchased = [
+    {
+      meter_number: "37185698745",
+      tokens: "1475-0553-400-5370-9209",
+      date: "2024-02-27",
+      units: "70.16",
+      amount: "2000",
+    },
+    {
+      meter_number: "37185698846",
+      tokens: "1675-0553-400-5370-9209",
+      date: "2024-02-25",
+      units: "5.26",
+      amount: "150",
+    },
+    {
+      meter_number: "37185698999",
+      tokens: "1575-0553-400-5370-9209",
+      date: "2024-02-23",
+      units: "5.61",
+      amount: "160",
+    },
+  ];
 
   return (
     <div>
       <div className="mb-3">
         <DatePicker maxDate={dayjs(new Date(), dateFormat)} />
       </div>
-      <PurchasedToken />
+      {tokenPurchased.map((item: PurchasedTokenTypes, key: number) => (
+        <PurchasedToken item={item} key={key} />
+      ))}
     </div>
   );
 };
